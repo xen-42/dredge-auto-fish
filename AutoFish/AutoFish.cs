9853f6251fb4d6dbed2e8cd81c889999159fa206
Nick <nicholasconnors@hotmail.ca>
2024-04-02 00:05:44 -0400

    Create mod

---
+++ AutoFish/AutoFish.cs
@@ -0,0 +1,26 @@
+﻿using HarmonyLib;
+using UnityEngine;
+using Winch.Core;
+
+namespace AutoFish;
+
+[HarmonyPatch]
+public class AutoFish : MonoBehaviour
+{
+    public void Awake()
+    {
+        WinchCore.Log.Debug($"{nameof(AutoFish)} has loaded!");
+
+        new Harmony(nameof(AutoFish)).PatchAll();
+    }
+
+    [HarmonyPostfix]
+    [HarmonyPatch(typeof(HarvestMinigame), nameof(HarvestMinigame.Update))]
+    public static void HarvestMinigame_Update(HarvestMinigame __instance)
+    {
+        if (__instance.isGameRunning)
+        {
+            __instance.progress = 1f;
+        }
+    }
+}
